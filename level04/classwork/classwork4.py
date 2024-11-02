82d18044a9626049d34495a356c32d71ddb2696d
Roumined <sandroxakartoxa2010@gmail.com>
2024-11-02 23:56:07 +0400

    fifth commit

---
+++ level04/classwork/classwork4.py
@@ -0,0 +1,57 @@
+word1 = "hello"
+word2 = "how are you?"
+word3 = "i'm fine!"
+word4 = "thank you!"
+
+num1 = 1
+num2 = 2
+num3 = 3
+num4 = 4
+
+fl1 = 1.1
+fl2 = 1.2
+fl3 = 1.3
+fl4 = 1.4
+
+print(10 + 5)
+print(10 - 5)
+print(10 * 5)
+print(10 ** 5)
+print(10 / 5)
+print(10 // 5)
+
+p = 10 + 5
+m = 10 - 5
+t = 10 * 5
+tt = 10 ** 5
+d = 10 / 5
+dd = 10 // 5
+
+print(p)
+print(m)
+print(t)
+print(tt)
+print(d)
+print(dd)
+
+pp = 10
+mm = 5
+ss = 40
+cc = 10
+aa = 50
+rr = 5 
+kk = 10
+oo = 5
+gg = 10
+jj = 5
+ee = 2
+tt = 4
+
+print(pp + mm)
+print(ss - cc)
+print(aa * rr)
+print(kk / oo)
+print(gg // jj)
+print(ee ** tt)
+
+print(2 + 4 * 2 / 2)
---
+++ level04/homework/homework1.py
@@ -0,0 +1,5 @@
+my_moms_age = 33
+my_dads_age = 39
+my_age = 14
+
+print(my_moms_age + my_dads_age + my_age)
