824cc7f0ba2c09f519d769a2731e23a9045d11d8
Roumined <sandroxakartoxa2010@gmail.com>
2024-12-12 22:01:59 +0400

    ninth commit

--- level08/classwork/classwork.py
+++ level08/classwork/classwork.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #input funcciit chven kopmiuters vagzavnit informacias
 name = input("Enter Your Name: ")
 print(name)
@@ -7,14 +6,4 @@ word1= input("Enter Random Word: ")
 word2= input("Enter Another Random Word: ")
 
 print(word1 + " " + word2)
-=======
-#input funcciit chven kopmiuters vagzavnit informacias
-name = input("Enter Your Name: ")
-print(name)
-
-word1= input("Enter Random Word: ")
-word2= input("Enter Another Random Word: ")
-
-print(word1 + " " + word2)
->>>>>>> 7be3c4d039e3a5ddbd00d0cafee8d6b5a57d6d97
 #output aris ase vtqvat shedegi (rasac vxedavt ekranze)
--- level08/homework/homework.py
+++ level08/homework/homework.py
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 name = input("Enter Your Name: ")
-=======
-name = input("Enter Your Name: ")
->>>>>>> 7be3c4d039e3a5ddbd00d0cafee8d6b5a57d6d97
 print("Hello" + " " + name + "!")
