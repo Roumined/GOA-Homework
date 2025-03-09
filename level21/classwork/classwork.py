35e91cb3393111e643471ffb7524a2cc29e2bdf5
Roumined <sandroxakartoxa2010@gmail.com>
2025-03-09 19:56:28 +0400

    twenty-second commit

---
+++ level21/classwork/classwork.py
@@ -0,0 +1,20 @@
+car_list = ['mercedes', 'bmw', 'ferrari', 'tesla', 'toyota', 'audi', 'opel', 'ford', 'mustang', 'volkswagen']
+print(car_list)
+
+food_list = ['burger', 'burrito', 'fry', 'popcorn', 'khawapuri', 'lobiani', 'khinkali', 'sosisi', 'pizza', 'ajabsandali']
+print(food_list)
+
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(num[1:8])
+print(num[4:10])
+
+print(num[0])
+print(num[1])
+print(num[2])
+print(num[3])
+print(num[4])
+print(num[5])
+print(num[6])
+print(num[7])
+print(num[8])
+print(num[9])
