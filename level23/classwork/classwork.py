5ba80d409bb92e17490511f5b8c38f79574fc355
Roumined <sandroxakartoxa2010@gmail.com>
2025-03-22 19:05:20 +0400

    twenty-fourth commit

---
+++ level23/classwork/classwork.py
@@ -0,0 +1,19 @@
+sia = [1, 2, 3, 4, 5]
+print(sia[0])
+print(sia[2])
+print(sia[4])
+
+num = [1, 3, 3, 4, 5, 6, 6, 8, 9, 12]
+num[1] = 2
+num[6] = 7
+num[9] = 10
+print(num)
+
+something = "helloween"
+print(something[0:5])
+print(something[5] + something[6] + something[2] + something[3])
+print(something[8] + something[4])
+print(something[8] + something[4] + something[5])
+
+goa = ["GOA", "MAGARIA", "NOVATORI", "IS", "THE", "VIRGIN", "CHAD" ,"BEST" ,"ACADEMY"]
+print(goa[0], goa[3], goa[4], goa[7], goa[8])
---
+++ level23/homework/homework3.py
@@ -0,0 +1,2 @@
+items = ["car", "airplane", "computer", "phone", "TV", "cup"]
+print(items[3:6])
